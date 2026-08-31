e9d26ac1b5befb27c9d1a8ae0a629ba98da52419
thedailyfit <theakhileshreddy07@gmail.com>
2026-08-31 19:50:57 +0530

    fix(api): fix compression import and add esModuleInterop for vercel monorepo build

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,7 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import helmet from 'helmet';
-import * as compression from 'compression';
+import compression from 'compression';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
